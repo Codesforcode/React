ac2558b1fce9ecf6e58989bc3da557847d8073ff
Akash Agrawal <akashji93685@gmail.com>
2026-07-28 00:26:39 +0530

    "double click functionality added"

--- bg-color-changer/src/App.jsx
+++ bg-color-changer/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
 function App() {
   const [color, setColor] = useState(
-    localStorage.getItem("bgColor") || "olive"
+    localStorage.getItem("bgColor") || "white"
   );
  useEffect(() => {
   localStorage.setItem("bgColor", color);
 }, [color]);
+const changeColor = (newColor) => {
+  if (color === newColor) {
+    setColor("white");
+  } else {
+    setColor(newColor);
+  }
+};
  
 
   return (
@@ -13,19 +20,19 @@ function App() {
   <div className="w-full h-screen duration-200" style={{backgroundColor : color}}></div>
   <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-2 px-2 ">
     <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3  py-2 rounded-3xl">
-      <button onClick={() => setColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+      <button onClick={() => changeColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"red"}}> red</button>
-        <button onClick={() => setColor("orange")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        <button onClick={() => changeColor("orange")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"orange"}}> orange</button>
-        <button onClick={() => setColor("blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        <button onClick={() => changeColor("blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"blue"}}> blue</button>
-        <button onClick={() => setColor("green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        <button onClick={() => changeColor("green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"green"}}> green</button>
-        <button onClick={() => setColor("pink")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        <button onClick={() => changeColor("pink")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"pink"}}> pink</button>
-        <button onClick={() => setColor("black")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        <button onClick={() => changeColor("black")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"black"}}> black</button>
-        <button onClick={() => setColor("yellow")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        <button onClick={() => changeColor("yellow")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"yellow"}}> yellow</button>
 
     </div>
