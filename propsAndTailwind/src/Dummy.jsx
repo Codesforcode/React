8ad2319ee53157e0fce511b02316e5626c4699c2
Akash Agrawal <akashji93685@gmail.com>
2026-07-27 23:37:42 +0530

    done

---
+++ propsAndTailwind/src/Dummy.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dummy = () => {
+  return (
+    <div className="bg-green-500 text-white p-4 rounded-lg">
+      Dummy
+    </div>
+  );
+};
+
+export default Dummy;
